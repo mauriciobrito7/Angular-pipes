0e5ac2987ff4b4f53a4139ff68a55916295363ad
Mauricio <maubot01@gmail.com>
2017-07-09 00:11:24 -0400

    nuevos pies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,19 @@ export class AppComponent {
   nombre = 'Mauricio';
   arreglo = [1,2,3,4,5,6,7,8,9];
   PI = Math.PI;
+  a = 0.234;
+  salario = 1234.5;
+  heroe = {
+    nombre:'Logan',
+    clave: 'Wolverine',
+    edad:500,
+    direccion:{
+      casa:'masion X'
+    }
+  }
+
+  valorDePromesa = new Promise((resolve, reject)=>{
+    setTimeout(()=>resolve('Llego la data!!'),3500);
+  });
+
 } 
